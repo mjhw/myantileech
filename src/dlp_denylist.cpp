aadcd45f4c130c168ec7d51f6e2a2d40986ff129
mjhw <371432590@qq.com>
2020-10-29 14:46:07 +0800

    增加 DenyList_UserHash_Soft

--- src/dlp_denylist.cpp
+++ src/dlp_denylist.cpp
@@ -12,16 +12,6 @@
 #include <pthread.h>
 #endif
 
-#ifdef DLP_WIN32
-#include <tchar.h>
-#else
-#include <stdio.h>
-#include <iconv.h>
-#endif
-
-#include <vector>
-#include <memory>
-
 #include "dlp_functions.h"
 
 #define DLP_PRIVATE__DLP_DENYLIST
@@ -55,112 +45,10 @@ BEGIN_PRIVATE_NAMESPACE
 XX_GENERATE_CODES(XX)
 #undef XX
 
-typedef std::array<TCHAR, 32 + 1> str32_t;
-static std::vector<str32_t> DLP_USERHASH_SOFT_ICASE_vec;
-
-#ifndef DLP_WIN32
-struct IConvClose {
-  void operator()(iconv_t *cd) { iconv_close(*cd); }
-};
-bool UTF32_TO_UTF8(const wchar_t *wcs, std::string &mbs) {
-  char buf[240];
-  char *inbuf, *outbuf;
-  size_t inbytesleft, outbytesleft;
-
-  mbs.clear();
-
-  iconv_t cd = iconv_open("UTF-8", "UTF-32LE");
-  if (cd == iconv_t(-1)) {
-    return false;
-  }
-
-  std::unique_ptr<iconv_t, IConvClose> autoclose(&cd);
-  inbuf = (char *)wcs;
-  inbytesleft = wcslen(wcs) * sizeof(wchar_t);
-
-  while (inbytesleft) {
-    size_t inbytesleft_ = std::min(sizeof(buf) / 6U, inbytesleft);
-    inbytesleft -= inbytesleft_;
-    outbuf = buf;
-    outbytesleft = sizeof(buf);
-    if (iconv(cd, &inbuf, &inbytesleft_, &outbuf, &outbytesleft) ==
-        size_t(-1)) {
-      return false;
-    }
-    mbs.append(buf, sizeof(buf) - outbytesleft);
-  }
-
-  return true;
-}
-#endif
-
-void SetUserHashDenyList(LPCTSTR filepath) {
-#ifdef DLP_WIN32
-  FILE *fp = _tfopen(filepath, _T("rb"), _SH_DENYWR);
-#else
-  FILE *fp;
-  {
-    std::string mbs;
-    if (!UTF32_TO_UTF8(filepath, mbs))
-      return;
-    fp = fopen(mbs.c_str(), "rb");
-  }
-#endif
-  if (NULL == fp) {
-    return;
-  }
-  std::unique_ptr<FILE, int (*)(FILE *)> autoclose(fp, &fclose);
-  DLP_USERHASH_SOFT_ICASE_vec.clear();
-  DLP_USERHASH_SOFT_ICASE_vec.reserve(20);
-  str32_t s = {_T('\0')};
-  int i = 0;
-  TCHAR ch;
-  bool read_eol = false;
-  while (fread(&ch, sizeof(ch), 1, fp) == 1) {
-    if (read_eol) {
-      read_eol = !(ch == _T('\r') || ch == _T('\n'));
-      continue;
-    }
-    if (ISSPACE(ch)) {
-      continue;
-    }
-    if (ISXDIGIT(ch)) {
-      s[i] = ch;
-      if (++i < ARRAY_COUNT(s) - 1) {
-        continue;
-      }
-      DLP_USERHASH_SOFT_ICASE_vec.push_back(s);
-    }
-    i = 0;
-    read_eol = true;
-  }
-  if (ferror(fp)) {
-    DLP_USERHASH_SOFT_ICASE_vec.clear();
-  } else if (!DLP_USERHASH_SOFT_ICASE_vec.empty()) {
-    DLP_USERHASH_SOFT_ICASE_data =
-        (const LPCTSTR *)DLP_USERHASH_SOFT_ICASE_vec.data();
-    DLP_USERHASH_SOFT_ICASE_size = DLP_USERHASH_SOFT_ICASE_vec.size();
-  }
-}
-
 void InitGlobalDenyList(void) {
 #define XX(dl)                                                                 \
-  dl##_data == NULL;                                                           \
-  dl##_size = 0;
-  XX_GENERATE_CODES(XX)
-#undef XX
-
-  try {
-    SetUserHashDenyList(DLP_USERHASH_DENYLIST_PATH);
-  } catch (...) {
-    // ;
-  }
-
-#define XX(dl)                                                                 \
-  if (dl##_data == NULL) {                                                     \
-    dl##_data = dl;                                                            \
-    dl##_size = ARRAY_COUNT(dl);                                               \
-  }
+  dl##_data = dl;                                                              \
+  dl##_size = ARRAY_COUNT(dl);
   XX_GENERATE_CODES(XX)
 #undef XX
 }
